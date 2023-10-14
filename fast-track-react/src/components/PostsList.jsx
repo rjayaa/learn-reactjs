60bae2636b448e224c92b7b64dafaa98523cd645
rjayaa <rjaya.lelana03@gmail.com>
2023-10-15 00:52:11 +0700

    get&post with handling state

--- fast-track-react/src/components/PostsList.jsx
+++ fast-track-react/src/components/PostsList.jsx
@@ -2,14 +2,27 @@ import classes from "./PostsList.module.css";
 import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  // data yang diterima useeffect berupa function dan array
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:8080/posts");
+      const restData = await response.json();
+      // if(!response.ok){} kalau fail pada saat fetch
+      setPosts(restData.posts);
+      setIsFetching(false);
+    }
 
+    fetchPosts();
+  }, []);
   function addPostHandler(postData) {
     fetch("http://localhost:8080/posts", {
-      // to get and send data
+      // to send data
       method: "POST",
       body: JSON.stringify(postData),
       headers: {
@@ -26,17 +39,22 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are not posts yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading Posts...</p>
         </div>
       )}
     </>
